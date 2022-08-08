33ae442d31bd90bcf8b6c6f56496b333405d1621
Rafael Giacomazzi <giacomazzirafael@gmail.com>
2022-08-08 15:48:48 -0300

    commiting updates to main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from textblob import TextBlob
 
-app = Flask('meu_app')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Minha primeira API'
+    return '<h1>Minha primeira API</h1>'
 
-app.run()
+@app.route('/sentimento/<frase>')
+def sentimento(frase):
+    tb = TextBlob(frase)
+    tb_en = tb.translate(from_lang='pt-br', to='en')
+    polaridade = tb_en.sentiment.polarity
+    return f'Polaridade: {polaridade}'
+
+
+app.run(debug=True)
